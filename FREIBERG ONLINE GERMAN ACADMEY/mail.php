d99c2ea0f191cac2a0d9eca9daced659991812fe
ehas1990 <ehas1990>
2022-05-12 09:15:48 +0530

    11-5-22 update 6.00AM

---
+++ FREIBERG ONLINE GERMAN ACADMEY/mail.php
@@ -0,0 +1,21 @@
+<?php
+	if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$phone=$_POST['phone'];
+		$msg=$_POST['msg'];
+        $opp=$_POST['opp'];
+
+		$to='ehasahamed@mail.com'; 
+		$subject='Freiberg Online German Academy';
+		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg."\n\n"."course opp:".$opp;
+		$headers="From: ".$email;
+
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
+?>
