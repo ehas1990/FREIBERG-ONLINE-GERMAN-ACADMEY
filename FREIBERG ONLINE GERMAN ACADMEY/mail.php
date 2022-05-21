b68157405eaeaa094c949dfbc0000121012f182d
ehas1990 <ehas1990>
2022-05-21 12:42:57 +0530

    21-5-22 new updation 12:42pm

--- FREIBERG ONLINE GERMAN ACADMEY/mail.php
+++ FREIBERG ONLINE GERMAN ACADMEY/mail.php
@@ -4,18 +4,17 @@
 		$email=$_POST['email'];
 		$phone=$_POST['phone'];
 		$msg=$_POST['msg'];
-        $opp=$_POST['opp'];
+        $sub=$_POST['sub'];
 
 		$to='ehasahamed@mail.com'; 
 		$subject='Freiberg Online German Academy & Educational Consultancy';
-		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg."\n\n"."course opp:".$opp;
-		$headers="From: ".$email;
-
-		if(mail($to, $subject, $message, $headers)){
-			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
-		}
-		else{
-			echo "Something went wrong!";
-		}
+		$txt ="Name : ". $name . "\r\n phone : ". $phone . "\r\n Email : " . $email ."\r\n sub : ". $sub . "\r\n Message :" . $message;
+           $headers = "From: $email" . "\r\n" .
+        "CC: somebodyelse@example.com";
+     if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
 	}
 ?>
